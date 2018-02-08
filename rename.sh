d45ed914c74010e5d63a6947ec74323a5c493e16
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-08 08:51:38 -0500

    Fix formatting for sh

--- rename.sh
+++ rename.sh
@@ -3,15 +3,15 @@ echo " " >> filerename.csv
 > output.log
 while read line
 do
-	old=$(echo "$line" | awk -F"," '{print $1}')
-	new=$(echo "$line" | awk -F"," '{print $2}')
-	for filename in $old*.jpg
-	do
-	if [ -f $filename ]; then
-	    mv -i "$filename" "${filename/$old/$new}"
+  old=$(echo "$line" | awk -F"," '{print $1}')
+  new=$(echo "$line" | awk -F"," '{print $2}')
+  for filename in $old*.jpg
+  do
+    if [ -f $filename ]; then
+      mv -i "$filename" "${filename/$old/$new}"
     else
-        echo "No filename containing '$old' found." >> output.log 2>&1
+      echo "No filename containing '$old' found." >> output.log 2>&1
     fi
-    done
+  done
 done < filerename.csv
 perl -pi -e 'chomp if eof' filerename.csv
