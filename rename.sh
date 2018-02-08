33ff8efd6bb60bd5fd1de9ad1724d820b61e0713
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-07 20:43:08 -0500

    Work for any filetype

--- rename.sh
+++ rename.sh
@@ -3,5 +3,5 @@ while read line
 do
 	old=$(echo "$line" | awk -F"," '{print $1}')
 	new=$(echo "$line" | awk -F"," '{print $2}')
-	for f in $old*.jpg; do mv -i "$f" "${f/$old/$new}"; done
+	for f in $old*.*; do mv -i "$f" "${f/$old/$new}"; done
 done < filerename.csv
